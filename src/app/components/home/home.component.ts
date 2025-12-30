42c43481d958dc0250a48250035217ac0f7cc5aa
sethuraman-g <sethuraman.ganesan@numentica-ui.com>
2025-12-30 15:38:52 +0530

    Enhance home component with AI capabilities and service offerings, update event tabs, and fix footer Instagram link

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,11 @@ import { Route, Router } from '@angular/router';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import * as AOS from 'aos';
 
+interface CommonCardDataType {
+  icon: string;
+  title: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,11 +16,62 @@ import * as AOS from 'aos';
 })
 export class HomeComponent {
   clientOptions!: OwlOptions;
-  clientOptionstwo!: OwlOptions
+  clientOptionstwo!: OwlOptions;
+  activeIndex = 0;
+  intervalId: any;
+  tabs = [
+    { label: 'Customer Data Platform',title: 'Elevate Member Loyalty:', content: 'Transform Credit Union Experiences with Cutting-Edge Data & AI Solutions!' },
+    { label: 'Accelerate Transcript Evaluation',title: 'Accelerate Transcript Evaluation', content: 'Harness AI for fast-tracking the admission process at scale.' },
+    { label: 'Virtual Assistant for Finance Teams',title: 'Your Virtual Assistant for Finance Teams', content: 'Simplify reconciliations, speed up reporting, and deliver real-time insights with AI.' },
+    { label: 'Fast-track Your Data Integration Efforts',title: 'Fast-track Your Data Integration Efforts', content: "Use Mahaaya's Data Accelerator Framework to halve your time-to-market!" },
+    { label: 'Modernize Analytics, Optimize Spend',title: 'Modernize Analytics, Optimize Spend', content: 'Make effective decisions and improve business agility while reducing costs by 50%.' },
+    { label: 'Detect Copyright at Scale',title: 'Detect Copyright at Scale:', content: 'Leverage AI to safeguard property management firms from compliance issues!' },
+    { label: 'Agentic Process Automation',title: 'Speed up the Loan Application Process:', content: 'Easy form filling with an intelligent assistant and seamless documentation.' },
+  ];
+
+  aiCapablitiesData: CommonCardDataType[] = [
+    { icon: '/assets/icon/ai/education.svg', title: 'Education & Universities' },
+    { icon: '/assets/icon/ai/financial.svg', title: 'Financial Institutions' },
+    { icon: '/assets/icon/ai/realEstate.svg', title: 'Real Estate Intelligence' },
+    { icon: '/assets/icon/ai/accounting.svg', title: 'Finance & Accounting' },
+    { icon: '/assets/icon/ai/retail.svg', title: 'Retail & E-commerce' },
+    { icon: '/assets/icon/ai/cyberSecurity.svg', title: 'Cybersecurity & SIEM' },
+    { icon: '/assets/icon/ai/copyRight.svg', title: 'Copyright Detection' },
+    { icon: '/assets/icon/ai/documentInteligent.svg', title: 'Document Intelligence' },
+    { icon: '/assets/icon/ai/conversationAi.svg', title: 'Conversational AI'},
+    { icon: '/assets/icon/ai/education.svg', title: 'Education & Universities' },
+    { icon: '/assets/icon/ai/financial.svg', title: 'Financial Institutions' },
+    { icon: '/assets/icon/ai/realEstate.svg', title: 'Real Estate Intelligence' },
+    { icon: '/assets/icon/ai/accounting.svg', title: 'Finance & Accounting' },
+    { icon: '/assets/icon/ai/retail.svg', title: 'Retail & E-commerce' },
+    { icon: '/assets/icon/ai/cyberSecurity.svg', title: 'Cybersecurity & SIEM' },
+    { icon: '/assets/icon/ai/copyRight.svg', title: 'Copyright Detection' },
+    { icon: '/assets/icon/ai/documentInteligent.svg', title: 'Document Intelligence' },
+    { icon: '/assets/icon/ai/conversationAi.svg', title: 'Conversational AI'}
+  ];
+
+  serviceOfferingsData: CommonCardDataType[] = [
+    { icon: '/assets/icon/ai/home.svg', title: 'ARCHITECTURE & AI Consulting' },
+    { icon: '/assets/icon/ai/AgenticAutomation.svg', title: 'Agentic Automation'},
+    { icon: '/assets/icon/ai/Accelerators.svg', title: 'Accelerators' },
+    { icon: '/assets/icon/ai/AzureAiSupport.svg', title: 'Azure AI Infrastructure Support' },
+    { icon: '/assets/icon/ai/IndustryExpertise.svg', title: 'Industry Expertise' },
+    { icon: '/assets/icon/ai/AI.svg', title: 'AI Adoption & Change Management' },
+    { icon: '/assets/icon/ai/rapid.svg', title: 'Rapid Prototyping' },
+    { icon: '/assets/icon/ai/ML.svg', title: 'ML & Al Engineering Staffing' },
+    { icon: '/assets/icon/ai/aiSolution.svg', title: 'AI Solution Design' },
+    { icon: '/assets/icon/ai/training.svg', title: 'Training & Adoptions' },
+    { icon: '/assets/icon/ai/aiSecurityGovernance.svg', title: 'AI Security & Governance' },
+    { icon: '/assets/icon/ai/llm.svg', title: 'LLM Testing Framework' }
+  ]
+
   constructor(@Inject(DOCUMENT) private _doc: Document, private router: Router) {
   }
   ngOnInit() {
     this.getCarouselOptions();
+    this.intervalId = setInterval(() => {
+      this.activeIndex = (this.activeIndex + 1) % this.tabs.length;
+    }, 4000);
     // AOS.init();
   }
 
@@ -99,4 +155,23 @@ export class HomeComponent {
       },
     };
   }
+
+  setActive(index: number) {
+    this.activeIndex = index;
+  }
+
+  getTabTransform(index: number): string {
+  const total = this.tabs.length;
+  const angle = (360 / total) * index;
+  const rad = (angle * Math.PI) / 180;
+
+  // ✅ Responsive radius
+  const isSmallScreen = window.innerWidth <= 574.98;
+  const radius = isSmallScreen ? 120 : 185;
+
+  const x = radius * Math.cos(rad);
+  const y = radius * Math.sin(rad);
+
+  return `translate(${x}px, ${y}px)`;
+}
 }
